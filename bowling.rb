c27cce11ea98bd06c27feb806720b167a6883bb2
David Yip <dvd.yip@gmail.com>
2014-01-23 00:07:38 -0500

    refactored with single line if

--- bowling.rb
+++ bowling.rb
@@ -1,5 +1,4 @@
 class Game
-#array values = pins knocked over
 
 	def score(roll_ary)
 
@@ -7,41 +6,41 @@ class Game
 		i = 0
 		frame = 0
 
-		#perfect case
-
 		while frame <= 9
-			if frame == 9
+		#	if frame == 9
 				#last frame
 				if roll_ary[i] == 10 #strike
-					frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1].to_i  + roll_ary[i+2].to_i
-					#final_score(frame_score_ary)
+					frame_score_ary[frame] = roll_ary[i].to_i + roll_ary[i+1].to_i  + roll_ary[i+2].to_i
+					i += 1 if frame < 9
 				elsif roll_ary[i] == 0 #gutter
 					frame_score_ary[frame] = 0
-					#final_score(frame_score_ary)
-		 		elsif (roll_ary[i].to_i + roll_ary[i+1].to_i < 10) #open frame
-		 			frame_score_ary[frame] = roll_ary[i].to_i + roll_ary[i+1].to_i
-		 			#final_score(frame_score_ary)
+					i += 1 if frame < 9
+
+		 		elsif (roll_ary[i] + roll_ary[i+1] < 10) #open frame
+		 			frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1]
+		 			i += 2 if frame < 9
+
 		 		elsif (roll_ary[i] + roll_ary[i+1] == 10) #spare
 		 			frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
-		 			final_score(frame_score_ary)
+		 			i += 2 if frame < 9
 				end
 
-			elsif frame < 9
+		#	elsif frame < 9
 				#previous frame handling
-				if roll_ary[i] == 10 #strike
-					frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
-					i += 1
-				elsif roll_ary[i] == 0 #gutter
-					frame_score_ary[frame] = 0
-					i += 1
-		 		elsif (roll_ary[i].to_i + roll_ary[i+1].to_i < 10) #open frame
-		 			frame_score_ary[frame] = roll_ary[i].to_i + roll_ary[i+1].to_i
-		 			i += 2
-		 		elsif (roll_ary[i] + roll_ary[i+1] == 10) #spare
-		 			frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
-		 			i += 2
-				end
-			end
+				# if roll_ary[i] == 10 #strike
+				# 	frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
+				# 	i += 1
+				# elsif roll_ary[i] == 0 #gutter
+				# 	frame_score_ary[frame] = 0
+				# 	i += 1
+		 	# 	elsif (roll_ary[i] + roll_ary[i+1] < 10) #open frame
+		 	# 		frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1]
+		 	# 		i += 2
+		 	# 	elsif (roll_ary[i] + roll_ary[i+1] == 10) #spare
+		 	# 		frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
+		 	# 		i += 2
+				#end
+			#end
 			frame += 1
 		end
 	final_score(frame_score_ary)
@@ -49,10 +48,8 @@ class Game
 
 	def final_score(frame_score_ary)
 		score = 0
-		frame_score_ary.each do |s|
-				score = score + s
-			end
-			return score
+		frame_score_ary.each { |s| score = score + s}
+		score
 	end
 end
 
