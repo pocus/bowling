44421c0fd6e69656307199a34252687435a7631e
David Yip <dvd.yip@gmail.com>
2014-01-23 00:25:01 -0500

    remove extra comments

--- bowling.rb
+++ bowling.rb
@@ -1,14 +1,11 @@
 class Game
 
 	def score(roll_ary)
-
 		frame_score_ary = []
 		i = 0
 		frame = 0
 
 		while frame <= 9
-		#	if frame == 9
-				#last frame
 				if roll_ary[i] == 10 #strike
 					frame_score_ary[frame] = roll_ary[i].to_i + roll_ary[i+1].to_i  + roll_ary[i+2].to_i
 					i += 1 if frame < 9
@@ -24,26 +21,10 @@ class Game
 		 			frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
 		 			i += 2 if frame < 9
 				end
-
-		#	elsif frame < 9
-				#previous frame handling
-				# if roll_ary[i] == 10 #strike
-				# 	frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
-				# 	i += 1
-				# elsif roll_ary[i] == 0 #gutter
-				# 	frame_score_ary[frame] = 0
-				# 	i += 1
-		 	# 	elsif (roll_ary[i] + roll_ary[i+1] < 10) #open frame
-		 	# 		frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1]
-		 	# 		i += 2
-		 	# 	elsif (roll_ary[i] + roll_ary[i+1] == 10) #spare
-		 	# 		frame_score_ary[frame] = roll_ary[i] + roll_ary[i+1] + roll_ary[i+2]
-		 	# 		i += 2
-				#end
-			#end
 			frame += 1
 		end
-	final_score(frame_score_ary)
+
+		final_score(frame_score_ary)
 	end
 
 	def final_score(frame_score_ary)
@@ -51,5 +32,5 @@ class Game
 		frame_score_ary.each { |s| score = score + s}
 		score
 	end
-end
 
+end
